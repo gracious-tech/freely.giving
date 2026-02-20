633335436c610c438fb0b607f3ffd234b51c7dcb
shadow-light <noreply@gracious.tech>
2026-02-20 16:09:14 +1100

    Add publishing advice for images

--- app/components/publish/PrepareImages.vue
+++ app/components/publish/PrepareImages.vue
@@ -1,7 +1,7 @@
 
 <template lang="pug">
 
-p For example, if your image is a digital illustration, then release the source file for the illustration, not just the final PNG or JPG.
+p For example, if your image is a digital illustration, then release the source file for the illustration, not just the final PNG or JPG. If it's a photo, simply publish the highest resolution of it.
 
 p If you are not sure where to put these files, simply creating a webpage or public Google Drive folder for them would do.
 
--- app/components/publish/ShareImages.vue
+++ app/components/publish/ShareImages.vue
@@ -1,6 +1,8 @@
 
 <template lang="pug">
 
-p Submit your images to #[GO(to='https://www.freebibleimages.org/') Free Bible Images].
+p Submit images that depict Bible passages to #[GO(to='https://www.freebibleimages.org/getinvolved/artists/') Free Bible Images].
+
+p If they are not Bible-related, you can submit them to public collections such as the #[GO(to='https://wordpress.org/photos/faq/') WordPress Photo Directory] or #[GO(to='https://commons.wikimedia.org/wiki/Commons:Contributing_your_own_work') Wikimedia Commons]. They will then automatically be discoverable by other collections like #[GO(to='https://openverse.org/') Openverse] (which we include on this website).
 
 </template>
